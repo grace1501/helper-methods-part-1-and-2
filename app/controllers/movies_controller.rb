f7e8872015e10140c450ef66130c711e3135afd7
Grace Do <61570746+grace1501@users.noreply.github.com>
2025-03-16 05:01:36 +0000

    rename to follow rails convention

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,16 +1,16 @@
 class MoviesController < ApplicationController
   def new
-    @the_movie = Movie.new
+    @movie = Movie.new
   end
 
   def index
     matching_movies = Movie.all
 
-    @list_of_movies = matching_movies.order({ :created_at => :desc })
+    @movies = matching_movies.order({ :created_at => :desc })
 
     respond_to do |format|
       format.json do
-        render json: @list_of_movies
+        render json: @movies
       end
 
       format.html do
@@ -20,20 +20,16 @@ class MoviesController < ApplicationController
   end
 
   def show
-    the_id = params.fetch(:id)
-
-    matching_movies = Movie.where({ :id => the_id })
-
-    @the_movie = matching_movies.first
+    @movie = Movie.find(params.fetch(:id))
   end
 
   def create
-    @the_movie = Movie.new
-    @the_movie.title = params.fetch("query_title")
-    @the_movie.description = params.fetch("query_description")
+    @movie = Movie.new
+    @movie.title = params.fetch(:title)
+    @movie.description = params.fetch(:description)
 
-    if @the_movie.valid?
-      @the_movie.save
+    if @movie.valid?
+      @movie.save
       redirect_to(movies_url, { :notice => "Movie created successfully." })
     else
       render template: "movies/new"
@@ -43,17 +39,17 @@ class MoviesController < ApplicationController
   def edit
     the_id = params.fetch(:id)
 
-    matching_movies = Movie.where({ :id => the_id })
+    matching_movies = Movie.where(id: the_id)
 
-    @the_movie = matching_movies.first
+    @movie = matching_movies.first
   end
 
   def update
     the_id = params.fetch(:id)
     the_movie = Movie.where({ :id => the_id }).first
 
-    the_movie.title = params.fetch("query_title")
-    the_movie.description = params.fetch("query_description")
+    the_movie.title = params.fetch("title")
+    the_movie.description = params.fetch("description")
 
     if the_movie.valid?
       the_movie.save
